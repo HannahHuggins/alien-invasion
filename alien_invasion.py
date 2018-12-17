5a0f7be8245262fb3983425f936056f025a7a5f8
Hannah huggins <dowdallhannah@yahoo.com>
2018-12-17 15:18:05 +0000

    Added bullets to game.

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,6 +4,7 @@ from ship import Ship
 from orca import Orca
 from settings import Settings
 import game_functions as gf
+from pygame.sprite import Group
 
 def run_game():
     # Initialize pygame, settings, and create a screen object.
@@ -17,15 +18,25 @@ def run_game():
     ship = Ship(ai_settings, screen)
     # Make an orca
     orca = Orca(ai_settings, screen)
+    # Make a group to store bullets in
+    bullets = Group()
 
     # Set the background color.
     bg_color = (0,255,170)
 
     # Start the main loop for the game.
     while True:
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, orca, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship, orca)
+        bullets.update()
+
+        # Get rid of bullets that have disappeared.
+        for bullet in bullets.copy():
+            if bullet.rect.bottom <= 0:
+                bullets.remove(bullet)
+        print(len(bullets))
+
+        gf.update_screen(ai_settings, screen, ship, orca, bullets)
 
         # Redraw the screen during each pass through the loop
         # screen.fill([255,255,255])
--- settings.py
+++ settings.py
@@ -9,4 +9,11 @@ class Settings:
         self.bg_color = (0,255,170)
 
         # Ship settings
-        self.ship_speed_factor = 1.5
+        self.ship_speed_factor = 2.5
+
+        # Bullet settings
+        self.bullet_speed_factor = 5
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 60,60,60
+        self.bullets_allowed = 10
