7fc965d674a9e0026131aa95525b1d2ef4ec5c07
Hannah huggins <dowdallhannah@yahoo.com>
2018-12-17 11:18:33 +0000

    Tidied up code and added further comments, added orca class.

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,34 +1,36 @@
 import sys
 import pygame
 from ship import Ship
+from orca import Orca
 from settings import Settings
 import game_functions as gf
 
-
-
-
-
 def run_game():
-    # Initialize pygame, settings, and screen object.
+    # Initialize pygame, settings, and create a screen object.
     pygame.init()
     ai_settings = Settings()
-    screen = pygame.display.set_mode(
+    screen = pygame.display.set_mode((1200, 800)
         (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
     # Make a ship
     ship = Ship(screen)
+    # Make an orca
+    orca = Orca(screen)
 
     # Set the background color.
-    bg_color = (230, 230, 230)
+    bg_color = (0,255,170)
 
     # Start the main loop for the game.
     while True:
-        gf.check_events()
+        gf.check_events(ship)
+        gf.update_screen(ai_settings, screen, ship, orca)
 
         # Redraw the screen during each pass through the loop
         screen.fill(ai_settings.bg_color)
+
         ship.blitme()
+        orca.blitme()
         # Watch for keyboard and mouse events.
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -37,5 +39,6 @@ def run_game():
 
         # Make the most recently drawn screen visible.
         pygame.display.flip()
+
 
 run_game()
--- game_functions.py
+++ game_functions.py
@@ -1,14 +1,23 @@
 import sys
 import pygame
 
-def check_events():
+def check_events(ship):
     """Respond to keypresses and mouse events."""
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
 
-def update_screen(ai_settings, screen, ship):
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                ship.moving_right = True
+
+
+def update_screen(ai_settings, screen, ship, orca):
     """ Update images on the screen and flip to the new screen"""
     # Redraw the screen during each pass through the loop.
     screen.fill(ai_settings.bg_color)
     ship.blitme()
+    orca.blitme()
 
     # Make the most recently drawn screen visible.
     pygame.display.flip()
